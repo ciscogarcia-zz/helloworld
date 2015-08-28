78f01df2109e581283b4c28d3ef11e2208908083
Cisco Garcia <cisco@ciscogarcia.net>
2015-08-28 14:57:23 -0400

    add datbase functionality

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 
 from admin_report import Admin_Report
-
+from DB import DB
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
     admin_report = Admin_Report()
-    return render_template("admin.html", admin_report=admin_report)
+    db = DB()
+    registered_users = db.get_registered_users()
+    return render_template("admin.html", admin_report=admin_report, registered_users=registered_users)
 
 if __name__ == '__main__':
     app.debug = True
